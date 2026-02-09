d8f008a243dfa3864158ce696902051090881775
mattygersh21 <dakotaseia@hotmail.com>
2026-02-09 00:19:03 -0600

    - Enhanced main.py with MCP integration and update project name in pyproject.toml. - Added async functionality to interact with MCP tools and modified dependencies for langchain-mcp-adapters-demo.

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import asyncio
+
 from dotenv import load_dotenv
-import os
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+import json
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI(model="gpt-4.1-nano")
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["./servers/math_server.py"]
+)
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            # tools = await session.list_tools()
+            # print(tools.model_dump_json(indent=2))
+            tools = await load_mcp_tools(session)
+            # print(tools)
+            tools_data = [
+                {"name": t.name, "description": t.description or ""}
+                for t in tools
+            ]
+            print(json.dumps(tools_data, indent=2))
 
+            agent = create_react_agent(llm,tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content='what is 54+2*3')]})
+            print(result['messages'][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
